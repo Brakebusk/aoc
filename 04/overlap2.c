0660c0a1041b569e08744979e209c04ef7871de4
Herman Bastesen <hermaba@uio.no>
2022-12-05 08:25:17 +0100

    04

---
+++ 04/overlap2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("[ERROR] Missing parameter <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  FILE *fp = NULL;
+  if ((fp = fopen(filename, "r")) == NULL) {
+      printf("[ERROR] Failed to open file %s\n", filename);
+      exit(EXIT_FAILURE);
+  }
+
+  int overlapping = 0;
+
+
+  char line[20];
+  while(fgets(line, 20, fp)) {
+
+    int e1Min, e1Max, e2Min, e2Max;
+    sscanf(line, "%d-%d,%d-%d", &e1Min, &e1Max, &e2Min, &e2Max);
+
+    if (e1Min <= e2Max && e1Max >= e2Min) {
+        overlapping++;
+    }
+  }
+
+  fclose(fp);
+
+  printf("part 1: %d\n", overlapping);
+}
