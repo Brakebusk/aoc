f28a06e3ed383bda2d2f7fdc5be07840b3efe61e
Herman Bastesen <hermaba@uio.no>
2022-12-09 10:52:59 +0100

    part 2

--- 09/rope.c
+++ 09/rope.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BOARDSIZE 500
-#define START 249
+#define BOARDSIZE 1000
+#define DEBUG 0
 
-void debug(int head[2], int tail[2], int visited[BOARDSIZE][BOARDSIZE]) {
+void debug(int visited[BOARDSIZE][BOARDSIZE]) {
   for (int col = BOARDSIZE -1; col >= 0; col--) {
     for (int row = 0 ; row < BOARDSIZE; row++) {
         printf("%c", visited[row][col] ? '#' : '.');
@@ -15,11 +15,8 @@ void debug(int head[2], int tail[2], int visited[BOARDSIZE][BOARDSIZE]) {
   printf("\n");
 }
 
-void follow(int head[2], int tail[2], int visited[BOARDSIZE][BOARDSIZE]) {
-  //printf("Head: (%d, %d) Tail: (%d, %d)\n", head[0], head[1], tail[0], tail[1]);
+void follow(int head[2], int tail[2], int visited[BOARDSIZE][BOARDSIZE], int mark) {
   if (abs(head[0] - tail[0]) <= 1 && abs(head[1] - tail[1]) <= 1) return;
-
-  //printf("Following\n");
 
   if (tail[0] == head[0] - 2) {
     tail[0]++;
@@ -35,31 +32,25 @@ void follow(int head[2], int tail[2], int visited[BOARDSIZE][BOARDSIZE]) {
     tail[1]--;
   }
 
-  //printf("Head: (%d, %d) Tail: (%d, %d)\n", head[0], head[1], tail[0], tail[1]);
-
-  visited[tail[0]][tail[1]] = 1;
-  //debug(head, tail, visited);
+  if (mark) visited[tail[0]][tail[1]] = 1;
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("[ERROR] Missing parameter <filename>\n");
-    exit(EXIT_FAILURE);
-  }
-
-  char *filename = argv[1];
-
+int simulate(char* filename, int knotCount) {
   FILE *fp = NULL;
   if ((fp = fopen(filename, "r")) == NULL) {
       printf("[ERROR] Failed to open file %s\n", filename);
       exit(EXIT_FAILURE);
   }
 
-  int head[2] = {START, START};
-  int tail[2] = {START, START};
+  int knots[knotCount][2];
+  for (int i = 0; i < knotCount; i++) {
+    knots[i][0] = BOARDSIZE / 2;
+    knots[i][1] = BOARDSIZE / 2;
+  }
+
   int visited[BOARDSIZE][BOARDSIZE];
   memset(visited, 0, BOARDSIZE * BOARDSIZE * sizeof(int));
-  visited[START][START] = 1;
+  visited[BOARDSIZE / 2][BOARDSIZE / 2] = 1;
 
   char line[10];
   while(fgets(line, 10, fp)) {
@@ -70,30 +61,34 @@ int main(int argc, char *argv[]) {
     switch (direction) {
       case 'U':
         for (int i = 0; i < magnitude; i++) {
-          //printf("Up\n");
-          head[1]++;
-          follow(head, tail, visited);
+          knots[0][1]++;
+          for (int i = 0; i < knotCount - 1; i++) {
+            follow(knots[i], knots[i + 1], visited, i == knotCount - 2);
+          }
         }
         break;
       case 'R':
         for (int i = 0; i < magnitude; i++) {
-          //printf("Right\n");
-          head[0]++;
-          follow(head, tail, visited);
+          knots[0][0]++;
+          for (int i = 0; i < knotCount - 1; i++) {
+            follow(knots[i], knots[i + 1], visited, i == knotCount - 2);
+          }
         }
         break;
       case 'D':
         for (int i = 0; i < magnitude; i++) {
-          //printf("Down\n");
-          head[1]--;
-          follow(head, tail, visited);
+          knots[0][1]--;
+          for (int i = 0; i < knotCount - 1; i++) {
+            follow(knots[i], knots[i + 1], visited, i == knotCount - 2);
+          }
         }
         break;
       case 'L':
         for (int i = 0; i < magnitude; i++) {
-          //printf("Left\n");
-          head[0]--;
-          follow(head, tail, visited);
+          knots[0][0]--;
+          for (int i = 0; i < knotCount - 1; i++) {
+            follow(knots[i], knots[i + 1], visited, i == knotCount - 2);
+          }
         }
         break;
       default:
@@ -109,10 +104,21 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  debug(head, tail, visited);
-  printf("Head: (%d, %d)\nTail: (%d, %d)\n", head[0], head[1], tail[0], tail[1]);
-
-  printf("Part 1: %d\n", visitedCount);
+  if (DEBUG) debug(visited);
+  if (DEBUG) printf("Head: (%d, %d)\nTail: (%d, %d)\n", knots[0][0], knots[0][1], knots[knotCount - 1][0], knots[knotCount - 1][1]);
 
   fclose(fp);
+  return visitedCount;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("[ERROR] Missing parameter <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  printf("Part 1: %d\n", simulate(filename, 2));
+  printf("Part 2: %d\n", simulate(filename, 10));
 }
