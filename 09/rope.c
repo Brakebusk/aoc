4f01cf131ac7583102278d5448b956b245171e26
Herman Bastesen <hermaba@uio.no>
2022-12-09 13:08:09 +0100

    It works

--- 09/rope.c
+++ 09/rope.c
@@ -6,7 +6,7 @@
 #define DEBUG 0
 #define LENGTH 10
 
-void debug(int visited[2][BOARDSIZE][BOARDSIZE]) {
+void debugVisited(int visited[2][BOARDSIZE][BOARDSIZE]) {
   for (int col = BOARDSIZE -1; col >= 0; col--) {
     for (int row = 0 ; row < BOARDSIZE; row++) {
         printf("%c", visited[1][row][col] ? '#' : '.');
@@ -16,22 +16,37 @@ void debug(int visited[2][BOARDSIZE][BOARDSIZE]) {
   printf("\n");
 }
 
+void debugSteps(int knots[LENGTH][2]) {
+  return;
+  for (int row = BOARDSIZE - 1 ; row >= 0; row--) {
+    for (int col = 0; col < BOARDSIZE; col++) {
+        int found = 0;
+        for (int i = 0; i < 10; i++) {
+          if (knots[i][0] == col && knots[i][1] == row) {
+            if (i == 0) {
+              printf("H");
+            } else if (i == 9) {
+              printf("T");
+            } else printf("%d", i);
+            found = 1;
+            break;
+          }
+        }
+        if (!found) printf(".");
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+}
+
 void follow(int head[2], int tail[2], int visited[2][BOARDSIZE][BOARDSIZE], int mark) {
   if (abs(head[0] - tail[0]) <= 1 && abs(head[1] - tail[1]) <= 1) return;
 
-  if (tail[0] == head[0] - 2) {
-    tail[0]++;
-    tail[1] = head[1];
-  } else if (tail[0] == head[0] + 2) {
-    tail[0]--;
-    tail[1] = head[1];
-  } else if (tail[1] == head[1] - 2) {
-    tail[0] = head[0];
-    tail[1]++;
-  } else if (tail[1] == head[1] + 2) {
-    tail[0] = head[0];
-    tail[1]--;
-  }
+  int deltaX = head[0] > tail[0] ? 1 : head[0] < tail[0] ? -1 : 0;
+  int deltaY = head[1] > tail[1] ? 1 : head[1] < tail[1] ? -1 : 0;
+
+  tail[0] += deltaX;
+  tail[1] += deltaY;
 
   if (mark >= 0) visited[mark][tail[0]][tail[1]] = 1;
 }
@@ -67,37 +82,43 @@ int main(int argc, char *argv[]) {
     int magnitude;
     sscanf(line, "%c %d", &direction, &magnitude);
 
+    if (DEBUG) printf("%c %d\n\n", direction, magnitude);
+
     switch (direction) {
       case 'U':
         for (int i = 0; i < magnitude; i++) {
           knots[0][1]++;
-          for (int j = 0; j < LENGTH - 1; j++) {
-            follow(knots[j], knots[j + 1], visited, !j ? 0 : j == LENGTH - 2 ? 1 : -1);
+          for (int j = 1; j < LENGTH; j++) {
+            follow(knots[j - 1], knots[j], visited, j == 1 ? 0 : j == LENGTH - 1 ? 1 : -1);
           }
+          if (DEBUG) debugSteps(knots);
         }
         break;
       case 'R':
         for (int i = 0; i < magnitude; i++) {
           knots[0][0]++;
-          for (int j = 0; j < LENGTH - 1; j++) {
-            follow(knots[j], knots[j + 1], visited, !j ? 0 : j == LENGTH - 2 ? 1 : -1);
+          for (int j = 1; j < LENGTH; j++) {
+            follow(knots[j - 1], knots[j], visited, j == 1 ? 0 : j == LENGTH - 1 ? 1 : -1);
           }
+          if (DEBUG) debugSteps(knots);
         }
         break;
       case 'D':
         for (int i = 0; i < magnitude; i++) {
           knots[0][1]--;
-          for (int j = 0; j < LENGTH - 1; j++) {
-            follow(knots[j], knots[j + 1], visited, !j ? 0 : j == LENGTH - 2 ? 1 : -1);
+          for (int j = 1; j < LENGTH; j++) {
+            follow(knots[j - 1], knots[j], visited, j == 1 ? 0 : j == LENGTH - 1 ? 1 : -1);
           }
+          if (DEBUG) debugSteps(knots);
         }
         break;
       case 'L':
         for (int i = 0; i < magnitude; i++) {
           knots[0][0]--;
-          for (int j = 0; j < LENGTH - 1; j++) {
-            follow(knots[j], knots[j + 1], visited, !j ? 0 : j == LENGTH - 2 ? 1 : -1);
+          for (int j = 1; j < LENGTH; j++) {
+            follow(knots[j - 1], knots[j], visited, j == 1 ? 0 : j == LENGTH - 1 ? 1 : -1);
           }
+          if (DEBUG) debugSteps(knots);
         }
         break;
       default:
@@ -115,7 +136,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (DEBUG) debug(visited);
+  if (DEBUG) debugVisited(visited);
   if (DEBUG) printf("Head: (%d, %d)\nTail: (%d, %d)\n", knots[0][0], knots[0][1], knots[9][0], knots[9][1]);
 
   fclose(fp);
