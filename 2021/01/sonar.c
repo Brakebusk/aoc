ded62497e49e929e1d861714520672eb0959a252
Herman Bastesen <hermaba@uio.no>
2022-12-15 17:09:05 +0100

    Start 2021-01

--- 2021/01/sonar.c
+++ 2021/01/sonar.c
@@ -3,6 +3,20 @@
 #include <string.h>
 #include <limits.h>
 
+struct Buffer {
+  int depths[5];
+  int top;
+};
+
+void add(struct Buffer* buffer, int value) {
+  buffer->top = (buffer->top + 1) % 5;
+  buffer->depths[buffer->top] = value;
+}
+
+int getPrev(struct Buffer* buffer, int offset) {
+  return buffer->depths[(buffer->top - offset) % 5];
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -17,17 +31,30 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
   }
 
-  int increasing = 0;
-  int prev = INT_MAX;
-  
+  int part1 = 0;
+  int part2 = 0;
+
+  struct Buffer prev;
+  prev.top = 0;
+  for (int i = 0; i < 3; i++) prev.depths[i] = INT_MAX;
+
   char line[10];
+  int lineIndex = 0;
   while(fgets(line, 10, fp)) {
     int depth;
     sscanf(line, "%d", &depth);
 
-    if (depth > prev) increasing++;
-    prev = depth;
+    if (depth > getPrev(&prev, 0)) part1++;
+
+    int common = getPrev(&prev, 0) + getPrev(&prev, 1);
+    printf("%d vs %d = %d\n", common + depth, common + getPrev(&prev, 2), common + depth > common + getPrev(&prev, 2));
+    if (lineIndex++ > 3 && common + depth > common + getPrev(&prev, 2)) part2++;
+
+    add(&prev, depth);
   }
 
-  printf("Part 1: %d\n", increasing);
+  fclose(fp);
+
+  printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
