7fbae50faba9a3d086fbf29b2d2f1bb44bb5fee8
Herman Bastesen <hermaba@uio.no>
2023-12-04 09:01:26 +0100

    Part 2

--- 2023/04/scratchcard.c
+++ 2023/04/scratchcard.c
@@ -18,8 +18,11 @@ int main(int argc, char *argv[]) {
 
   int part1 = 0;
 
+  int copies[256] = {0};
+
   char line[256];
   while(fgets(line, 256, fp)) {
+    int winCount = 0;
     int points = 0;
     int winning[100] = {0};
     
@@ -27,6 +30,7 @@ int main(int argc, char *argv[]) {
     token = strtok(line, ":");
     int cardId;
     sscanf(token, "Card %d", &cardId);
+    copies[cardId]++;
     token = strtok(NULL, "|");
 
     // Mark winning numbers
@@ -42,6 +46,7 @@ int main(int argc, char *argv[]) {
     while ((segment = strtok_r(rest, " ", &rest))) {
       int value = atoi(segment);
       if (winning[value]) {
+        winCount++;
         if (points) {
           points *= 2;
         } else {
@@ -50,9 +55,17 @@ int main(int argc, char *argv[]) {
       }
     }
     part1 += points;
+
+    for (int i = 0; i < winCount; i++) {
+      copies[1 + cardId + i] += copies[cardId];
+    }
   }
   fclose(fp);
 
+  int part2 = 0;
+  for (int i = 0; i < sizeof(copies) / sizeof(int); i++) part2 += copies[i];
+
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
 
