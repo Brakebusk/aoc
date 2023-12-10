e8c5c3eef0fedf40e839416ee1c412d50391775d
Herman Bastesen <hermaba@uio.no>
2023-12-10 13:48:01 +0100

    Part 2

--- 2023/10/pipe.c
+++ 2023/10/pipe.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 struct tile {
+  char symbol;
   char connections[4];
   int distance;
 };
@@ -29,6 +30,17 @@ void move(int location[2], char direction) {
 
 int invertDirection(char direction) {
   return (direction + 2) % 4;
+}
+
+char connectionsToSymbol(char connections[4]) {
+  if (connections[0] && connections[2]) return '|';
+  if (connections[1] && connections[3]) return '-';
+  if (connections[0] && connections[1]) return 'L';
+  if (connections[0] && connections[3]) return 'J';
+  if (connections[2] && connections[3]) return '7';
+  if (connections[1] && connections[2]) return 'F';
+  printf("Invalid connections\n");
+  exit(EXIT_FAILURE);
 }
 
 int main(int argc, char *argv[]) {
@@ -110,6 +122,7 @@ int main(int argc, char *argv[]) {
           printf("Unknown character '%c'\n", line[c]);
           exit(EXIT_FAILURE);
       }
+      grid[lineIndex][c].symbol = line[c];
     }
 
     lineIndex++;
@@ -120,6 +133,7 @@ int main(int argc, char *argv[]) {
   if (start[0] < gridSize - 1 && grid[start[0]+1][start[1]].connections[0]) grid[start[0]][start[1]].connections[2] = 1;
   if (start[1] > 0 && grid[start[0]][start[1]-1].connections[1]) grid[start[0]][start[1]].connections[3] = 1;
   if (start[1] < gridSize - 1 && grid[start[0]][start[1]+1].connections[3]) grid[start[0]][start[1]].connections[1] = 1;
+  grid[start[0]][start[1]].symbol = connectionsToSymbol(grid[start[0]][start[1]].connections);
 
   for (int d = 0; d < 4; d++) {
     if (grid[start[0]][start[1]].connections[d]) {
@@ -154,5 +168,30 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  int part2 = 0;
+  for (int l = 0; l < gridSize; l++) {
+    int inside = 0;
+    int wallCount = 0;
+    char prev = -1;
+    for (int r = 0; r < gridSize; r++) {
+      struct tile current = grid[l][r];
+      
+      if (current.distance >= 0 && current.symbol != '-') {
+        wallCount++;
+        if ((current.symbol == 'J' && prev == 'F') || (current.symbol == '7' && prev == 'L')) {
+          wallCount--;
+        }
+        prev = current.symbol;
+        inside = wallCount % 2 == 1;
+      }
+      
+      if (inside && current.distance == -1) {
+        part2++;
+      }
+    }
+  }
+
+
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
