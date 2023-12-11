e77f22610a3c750c4f1142b787064748ccc63fbb
Herman Bastesen <hermaba@uio.no>
2023-12-11 09:00:05 +0100

    Part 2

--- 2023/11/cosmic.c
+++ 2023/11/cosmic.c
@@ -2,20 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define PART_2_FACTOR 1000000 - 1
+
 struct coordinate {
   int row, column;
 };
 
-void downShift(char grid[256][256], int fromRow, int gridHeight) {
-  for (int r = gridHeight; r > fromRow; r--) {
-    memcpy(grid[r], grid[r-1], gridHeight);
-  }
+int max(int a, int b) {
+  return a > b ? a : b;
 }
 
-void rightShift(char grid[256][256], int fromColumn, int gridWidth, int gridHeight) {
-  for (int c = gridWidth; c > fromColumn; c--) {
-    for (int r = 0; r < gridHeight; r++) grid[r][c] = grid[r][c-1];
-  }
+int min(int a, int b) {
+  return a < b ? a : b;
 }
 
 int main(int argc, char *argv[]) {
@@ -33,81 +31,82 @@ int main(int argc, char *argv[]) {
   }
 
   char grid[256][256];
-  int gridWidth = 0;
-  int gridHeight;
+  int gridSize = 0;
 
   char line[256];
   int row = 0;
   while(fgets(line, 256, fp)) {
-    if (!gridWidth) gridWidth = strlen(line) - 1;
-    memcpy(grid[row++], line, gridWidth);
+    if (!gridSize) gridSize = strlen(line) - 1;
+    memcpy(grid[row++], line, gridSize);
   }
   fclose(fp);
-  gridHeight = gridWidth;
-
-  // Expand rows
-  for (int r = 0; r < gridHeight; r++) {
-    int isEmpty = 1;
-    for (int c = 0; c < gridWidth; c++) {
-      if (grid[r][c] == '#') {
-        isEmpty = 0;
-        break;
-      }
-    }
-    if (isEmpty) {
-      downShift(grid, r++, gridHeight);
-      gridHeight++;
-    }
-  }
-
-  // Expand columns
-  for (int c = 0; c < gridWidth; c++) {
-    int isEmpty = 1;
-    for (int r = 0; r < gridHeight; r++) {
-      if (grid[r][c] == '#') {
-        isEmpty = 0;
-        break;
-      }
-    }
-    if (isEmpty) {
-      rightShift(grid, c++, gridWidth, gridHeight);
-      gridWidth++;
-    }
-  }
 
   struct coordinate galaxies[1024];
   int galaxyCount = 0;
 
-  for (int r = 0; r < gridHeight; r++) {
-    for (int c = 0; c < gridWidth; c++) {
+  int emptyRows[1024];
+  int emptyRowCount = 0;
+
+  for (int r = 0; r < gridSize; r++) {
+    int isEmpty = 1;
+    for (int c = 0; c < gridSize; c++) {
       if (grid[r][c] == '#') {
+        isEmpty = 0;
         galaxies[galaxyCount++] = (struct coordinate) {
           .row = r,
           .column = c
         };
       }
     }
+    if (isEmpty) {
+      emptyRows[emptyRowCount++] = r;
+    }
+  }
+
+  int emptyColumns[1024];
+  int emptyColumnCount = 0;
+
+  for (int c = 0; c < gridSize; c++) {
+    int isEmpty = 1;
+    for (int r = 0; r < gridSize; r++) {
+      if (grid[r][c] == '#') {
+        isEmpty = 0;
+        break;
+      }
+    }
+    if (isEmpty) {
+      emptyColumns[emptyColumnCount++] = c;
+    }
   }
 
   int part1 = 0;
+  long long part2 = 0;
+
   for (int g1 = 0; g1 < galaxyCount; g1++) {
     for (int g2 = g1 + 1; g2 < galaxyCount; g2++) {
       struct coordinate loc1 = galaxies[g1];
       struct coordinate loc2 = galaxies[g2];
-      part1 += abs(loc1.row - loc2.row) + abs(loc1.column - loc2.column);
+      int taxiCab = abs(loc1.row - loc2.row) + abs(loc1.column - loc2.column);
+      part1 += taxiCab;
+      part2 += taxiCab;
+
+      for (int eR = 0; eR < emptyRowCount; eR++) {
+        int row = emptyRows[eR];
+        if (max(loc1.row, loc2.row) > row && min(loc1.row, loc2.row) < row) {
+          part1 += 1;
+          part2 += PART_2_FACTOR;
+        }
+      }
+      for (int eC = 0; eC < emptyColumnCount; eC++) {
+        int column = emptyColumns[eC];
+        if (max(loc1.column, loc2.column) > column && min(loc1.column, loc2.column) < column) {
+          part1 += 1;
+          part2 += PART_2_FACTOR;
+        }
+      }
     }
   }
 
   printf("Part 1: %d\n", part1);
-  
-  /*
-  for (int r = 0; r < gridHeight; r++) {
-    for (int c = 0; c < gridWidth; c++) {
-      printf("%c", grid[r][c]);
-    }
-    printf("\n");
-  }
-  */
-
-
+  printf("Part 2: %lld\n", part2);
 }
