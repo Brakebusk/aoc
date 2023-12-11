b1aad45f6b28ed7139fbef377d728e1fe8f76461
Herman Bastesen <hermaba@uio.no>
2023-12-11 13:58:12 +0100

    2021/03 part 2

--- 2021/03/binary.c
+++ 2021/03/binary.c
@@ -6,6 +6,11 @@ int setBit(int num, int index) {
   return num | (1 << index);
 }
 
+void printNumber(char digits[], int n) {
+  for (int i = 0; i < n; i++) printf("%d", digits[i]);
+  printf("\n");
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -51,4 +56,84 @@ int main(int argc, char *argv[]) {
   }
 
   printf("Part 1: %d\n", gamma * epsilon);
+
+  int oxygenCandidates[1024];
+  int oxygenCandidateCount = numCount;
+  for (int i = 0; i < numCount; i++) oxygenCandidates[i] = 1;
+
+  for (int c = 0; c < numLength; c++) {
+    int sum = 0;
+    for (int r = 0; r < numCount; r++) {
+      if (oxygenCandidates[r]) sum += numbers[r][c];
+    }
+    if(sum >= oxygenCandidateCount - sum) {
+      for (int r = 0; r < numCount; r++) {
+        if (oxygenCandidates[r] && !numbers[r][c]) {
+          oxygenCandidates[r] = 0;
+          oxygenCandidateCount--;
+        }
+      }
+    } else {
+      for (int r = 0; r < numCount; r++) {
+        if (oxygenCandidates[r] && numbers[r][c]) {
+          oxygenCandidates[r] = 0;
+          oxygenCandidateCount--;
+        }
+      }
+    }
+    if (oxygenCandidateCount == 1) break;
+  }
+
+  int oxygen = 0;
+  for (int cand = 0; cand < numCount; cand++) {
+    if (oxygenCandidates[cand]) {
+      for (int c = 0; c < numLength; c++) {
+        if (numbers[cand][c]) {
+          oxygen = setBit(oxygen, numLength - 1 - c);
+        }
+      }
+      break;
+    }
+  }
+
+  int co2Candidates[1024];
+  int co2CandidateCount = numCount;
+  for (int i = 0; i < numCount; i++) co2Candidates[i] = 1;
+
+  for (int c = 0; c < numLength; c++) {
+    int sum = 0;
+    for (int r = 0; r < numCount; r++) {
+      if (co2Candidates[r]) sum += numbers[r][c];
+    }
+    if(sum >= co2CandidateCount - sum) {
+      for (int r = 0; r < numCount; r++) {
+        if (co2Candidates[r] && numbers[r][c]) {
+          co2Candidates[r] = 0;
+          co2CandidateCount--;
+        }
+      }
+    } else {
+      for (int r = 0; r < numCount; r++) {
+        if (co2Candidates[r] && !numbers[r][c]) {
+          co2Candidates[r] = 0;
+          co2CandidateCount--;
+        }
+      }
+    }
+    if (co2CandidateCount == 1) break;
+  }
+
+  int co2 = 0;
+  for (int cand = 0; cand < numCount; cand++) {
+    if (co2Candidates[cand]) {
+      for (int c = 0; c < numLength; c++) {
+        if (numbers[cand][c]) {
+          co2 = setBit(co2, numLength - 1 - c);
+        }
+      }
+      break;
+    }
+  }
+
+  printf("Part 2: %d\n", oxygen * co2);
 }
