6ddc4299d7bef7e2de5815f05bfc8d9345c445b1
Herman Bastesen <hermaba@uio.no>
2023-12-13 14:51:08 +0100

    2016/01

--- 2016/01/taxicab.c
+++ 2016/01/taxicab.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+int mod(int a, int b) {
+  return (a % b + b) % b;
+}
+
+int turn(int dir, int delta) {
+  return mod(dir + delta, 4);
+}
+
+int distance(int x, int y) {
+  return abs(x) + abs(y);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -19,4 +31,53 @@ int main(int argc, char *argv[]) {
   char line[1024];
   fgets(line, 1024, fp);
   fclose(fp);
+
+  char visited[1024][1024] = {0};
+  visited[0][0] = 1;
+
+  int part2 = 0;
+
+  int x = 0, y = 0, dir = 0;
+  char *token = 0;
+  while ((token = strtok(token ? NULL : line, ", "))) {
+    dir = turn(dir, token[0] == 'R' ? 1 : -1);
+    int magnitude = atoi(&token[1]);
+
+    switch (dir) {
+      case 0:
+        while (magnitude--) {
+          y += 1;
+          if (visited[x][y] && !part2) part2 = distance(x, y);
+          visited[x][y] = 1;
+        }
+        break;
+      case 1:
+        while (magnitude--) {
+          x += 1;
+          if (visited[x][y] && !part2) part2 = distance(x, y);
+          visited[x][y] = 1;
+        }
+        break;
+      case 2:
+        while (magnitude--) {
+          y -= 1;
+          if (visited[x][y] && !part2) part2 = distance(x, y);
+          visited[x][y] = 1;
+        }
+        break;
+      case 3:
+        while (magnitude--) {
+          x -= 1;
+          if (visited[x][y] && !part2) part2 = distance(x, y);
+          visited[x][y] = 1;
+        }
+        break;
+      default:
+        printf("Invalid direction: %d\n", dir);
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  printf("Part 1: %d\n", distance(x, y));
+  printf("Part 2: %d\n", part2);
 }
