b20d43c1ddc2f7488314bec06760a156bc9fe77e
Herman Bastesen <hermaba@uio.no>
2023-12-28 11:01:50 +0100

    2019/16 brute force xd

--- 2019/16/fft.c
+++ 2019/16/fft.c
@@ -9,8 +9,8 @@ int getPatternValue(int digit, int index) {
   return basePattern[((index + 1) / repeats) % 4];
 }
 
-void printDigits(int digits[800], int digitCount) {
-  for (int i = 0; i < digitCount; i++) printf("%d", digits[i]);
+void printDigits(int *digits, int digitCount, int offset) {
+  for (int i = offset; i < digitCount+offset; i++) printf("%d", digits[i]);
   printf("\n");
 }
 
@@ -32,25 +32,37 @@ int main(int argc, char *argv[]) {
   fgets(line, 800, fp);
   fclose(fp);
 
-  int digits[800];
   int digitCount = strlen(line);
+  int *digits = malloc(digitCount * sizeof(int) * 10001);
   for (int d = 0; d < digitCount; d++) digits[d] = line[d] - '0';
+  for (int r = 0; r < 10000; r++) {
+    memcpy(&digits[(r+1) * digitCount], digits, sizeof(int) * digitCount);
+  }
 
-  int output[800];
+  int *output = malloc(digitCount * sizeof(int) * 10001);
+
+  char offsetDigits[8];
+  strncpy(offsetDigits, line, 7);
+  int offset = atoi(offsetDigits);
+
+  int trueDigitCount = digitCount*10000;
 
   for (int i = 0; i < 100; i++) {
-    for (int d = 0; d < digitCount; d++) {
+    printf("Round #%d\n", i+1);
+    for (int d = 0; d < trueDigitCount; d++) {
+      if (d % 10000 == 0) printf("  -> digit %d\n", d);
       int v = 0;
-      for (int j = 0; j < digitCount; j++) {
+      for (int j = 0; j < trueDigitCount; j++) {
         v += getPatternValue(d, j) * digits[j];
       }
       output[d] = abs(v) % 10;
     }
 
-    for (int d = 0; d < digitCount; d++) {
-      digits[d] = output[d];
-    }
+    memcpy(digits, output, trueDigitCount * sizeof(int));
   }
   printf("Part 1: ");
-  printDigits(digits, 8);
+  printDigits(digits, 8, 0);
+  printf("Part 2 (offset %d): ", offset);
+  printDigits(digits, 8, offset);
+  free(digits);
 }
