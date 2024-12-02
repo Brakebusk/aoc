b4b0b6bd8624792165595598799ed50a2b98b58f
Herman Bastesen <hermaba@uio.no>
2024-12-02 09:30:58 +0100

    2024/02 part 2

--- 2024/02/reactor.c
+++ 2024/02/reactor.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+int check(int values[], int length) {
+  // Negative if increasing
+  int direction = 0;
+  for (int i = 0; i < length - 1; i++) {
+    if (direction == 0) {
+      direction = values[i] - values[i+1];
+    }
+
+    int diff = abs(values[i] - values[i+1]);
+    
+    if (diff > 3 || diff < 1) return 0;
+    if (direction < 0 && values[i] > values[i+1]) return 0;
+    if (direction > 0 && values[i] < values[i+1]) return 0;
+  }
+
+  return 1;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -17,6 +35,7 @@ int main(int argc, char *argv[]) {
   }
 
   int part1 = 0;
+  int part2 = 0;
 
   char line[32];
   while(fgets(line, 32, fp)) {
@@ -29,33 +48,26 @@ int main(int argc, char *argv[]) {
       token = strtok(NULL, " ");
     }
 
-    // Negative if increasing
-    int direction = 0;
-    int valid = 1;
-    for (int i = 0; i < length - 1; i++) {
-      if (direction == 0) {
-        direction = values[i] - values[i+1];
+    int valid = check(values, length);
+    if (valid) {
+      part1++;
+      part2++;
+    } else {
+      for (int remove = 0; remove < length; remove++) {
+        int copyLength = 0;
+        int copy[16];
+        for (int i = 0; i < length; i++) {
+          if (i != remove) copy[copyLength++] = values[i];
+        }
+        if (check(copy, copyLength)) {
+          part2++;
+          break;
+        }
       }
-
-      int diff = abs(values[i] - values[i+1]);
-      if (diff > 3 || diff < 1) {
-        valid = 0;
-        break;
-      }
-
-      if (direction < 0 && values[i] > values[i+1]) {
-        valid = 0;
-        break;
-      }
-      if (direction > 0 && values[i] < values[i+1]) {
-        valid = 0;
-        break;
-      }
-
     }
-    part1 += valid;
   }
   fclose(fp);
 
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
