77cdff936683f6729780e398d35ef38962f9f717
Herman Bastesen <hermaba@uio.no>
2024-12-11 13:07:09 +0100

    2016/03 part 2

--- 2016/03/triangle.c
+++ 2016/03/triangle.c
@@ -6,6 +6,12 @@ int compare(const void* a, const void* b) {
    return (*(int*)a - *(int*)b);
 }
 
+int check(int a, int b, int c) {
+  int values[3] = {a, b, c};
+  qsort(values, 3, sizeof(int), compare);
+  return values[0] + values[1] > values[2];
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -20,16 +26,29 @@ int main(int argc, char *argv[]) {
       exit(EXIT_FAILURE);
   }
 
+  int grid[2000][3];
+  int length = 0;
+
   int part1 = 0;
+  int part2 = 0;
 
   char line[20];
   while(fgets(line, 20, fp)) {
-    int values[3];
-    sscanf(line, "%d %d %d", &values[0], &values[1], &values[2]);
-    qsort(values, 3, sizeof(int), compare);
-    if (values[0] + values[1] > values[2]) part1++;
+    int a, b, c;
+    sscanf(line, "%d %d %d", &a, &b, &c);
+    grid[length][0] = a;
+    grid[length][1] = b;
+    grid[length++][2] = c;
+    part1 += check(a, b, c);
   }
   fclose(fp);
 
+  for (int r = 0; r < length; r += 3) {
+    part2 += check(grid[r][0], grid[r+1][0], grid[r+2][0]) +
+             check(grid[r][1], grid[r+1][1], grid[r+2][1]) + 
+             check(grid[r][2], grid[r+1][2], grid[r+2][2]);
+  }
+
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
