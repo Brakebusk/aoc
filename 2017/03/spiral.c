a10056097c8bb3653583d2dc3d7c0e72312b3f4c
Herman Bastesen <hermaba@uio.no>
2024-12-11 13:55:03 +0100

    2017/03 part 2

--- 2017/03/spiral.c
+++ 2017/03/spiral.c
@@ -2,20 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define INPUT 289326
+
 int distance(int x, int y) {
   return abs(x) + abs(y);
 }
 
-int main(int argc, char *argv[]) {
+int getAdjacentSum(int spiral[1000][1000], int r, int c) {
+  return spiral[r-1][c-1] + spiral[r-1][c] + spiral[r-1][c+1] + 
+         spiral[r][c-1] + spiral[r][c] + spiral[r][c+1] + 
+         spiral[r+1][c-1] + spiral[r+1][c] + spiral[r+1][c+1];
+}
 
+int getSpiral(int part) {
   int spiral[1000][1000];
   memset(spiral, 0, sizeof(int) * 1000 * 1000);
   int center = 499;
+  spiral[center][center] = 1;
 
   int r = center, c = center, dir = 2, counter = 1;
   while(1) {
-    spiral[r][c] = counter++;
-    if (spiral[r][c] == 289326) break;
+    spiral[r][c] = part == 1 ? counter++ : getAdjacentSum(spiral, r, c);
+    if (part == 1 && spiral[r][c] == INPUT) return distance(r-center, c-center);
+    if (part == 2 && spiral[r][c] > INPUT) return spiral[r][c];
 
     switch(dir) {
       case 0:
@@ -48,5 +57,9 @@ int main(int argc, char *argv[]) {
     }
   };
 
-  printf("Part 1: %d\n", distance(r-center, c-center));
+}
+
+int main(int argc, char *argv[]) {
+  printf("Part 1: %d\n", getSpiral(1));
+  printf("Part 2: %d\n", getSpiral(2));
 }
