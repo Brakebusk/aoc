d2b7d1c4036f649b4c2e5061551f32a25d298c33
Herman Bastesen <hermaba@uio.no>
2024-12-13 08:06:18 +0100

    Use same method for part 1

--- 2024/13/claw.c
+++ 2024/13/claw.c
@@ -7,6 +7,19 @@ struct machine {
   long long adx, ady, bdx, bdy, x, y;
 };
 
+long long solve(struct machine m) {
+  long long numeratorX = m.x * m.bdy - m.y * m.bdx;
+  long long denomiatorX = m.adx * m.bdy - m.ady * m.bdx;
+      
+  long long numeratorY = m.x * m.ady - m.y * m.adx;
+  long long denomiatorY = m.ady * m.bdx - m.adx * m.bdy;
+
+  if (numeratorX % denomiatorX == 0 && numeratorY % denomiatorY == 0) {
+    return 3 * (numeratorX / denomiatorX) + (numeratorY / denomiatorY);
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -56,29 +69,10 @@ int main(int argc, char *argv[]) {
   long long part2 = 0;
   for (int i = 0; i < count; i++) {
     struct machine m = machines[i];
-    long long minTokens = LLONG_MAX;
-    for (int a = 0; a < 101; a++) {
-      for (int b = 0; b < 101; b++) {
-        if (m.adx * a + m.bdx * b == m.x && m.ady * a + m.bdy * b == m.y) {
-          long long tokens = 3 * a + b;
-          if (tokens < minTokens) minTokens = tokens;
-        }
-      }
-    }
-    if (minTokens < LLONG_MAX) part1 += minTokens;
-
-    long long p2x = m.x + 10000000000000;
-    long long p2y = m.y + 10000000000000;
-    
-    long long numeratorX = p2x * m.bdy - p2y * m.bdx;
-    long long denomiatorX = m.adx * m.bdy - m.ady * m.bdx;
-        
-    long long numeratorY = p2x * m.ady - p2y * m.adx;
-    long long denomiatorY = m.ady * m.bdx - m.adx * m.bdy;
-
-    if (numeratorX % denomiatorX == 0 && numeratorY % denomiatorY == 0) {
-      part2 += 3 * (numeratorX / denomiatorX) + (numeratorY / denomiatorY);
-    }
+    part1 += solve(m);
+    m.x += 10000000000000;
+    m.y += 10000000000000;
+    part2 += solve(m);
   }
 
   printf("Part 1: %lld\n", part1);
