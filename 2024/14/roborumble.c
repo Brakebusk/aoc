d76d8685f146cba9dacfc6a235e29b3d370b1a8c
Herman Bastesen <hermaba@uio.no>
2024-12-14 14:12:46 +0100

    2024/14 part 2

--- 2024/14/roborumble.c
+++ 2024/14/roborumble.c
@@ -16,6 +16,47 @@ int mod(int a, int b) {
   return (a % b + b) % b;
 }
 
+void generateImage(struct robot robots[512], int count, int seconds) {
+  int matrix[HEIGHT][WIDTH];
+  memset(matrix, 0, sizeof(int) * HEIGHT * WIDTH);
+  for (int i = 0; i < count; i++) matrix[robots[i].r][robots[i].c]++;
+
+
+  int size = WIDTH * HEIGHT * 4;
+
+  char header[54] = {0};
+  strcpy(header, "BM");
+  memset(&header[2],  (int)(54 + size), 1);
+  memset(&header[10], (int)54, 1);
+  memset(&header[14], (int)40, 1);
+  memset(&header[18], (int)WIDTH, 1);
+  memset(&header[22], (int)HEIGHT, 1);
+  memset(&header[26], (short)1, 1);
+  memset(&header[28], (short)32, 1);
+  memset(&header[34], (int)size, 1);
+
+  unsigned char *pixels = malloc(size);
+  for(int r = HEIGHT - 1; r >= 0; r--) {
+      for(int c = 0; c < WIDTH; c++) {
+          int p = (r * WIDTH + c) * 4;
+          int value = matrix[r][c] ? 255 : 0;
+
+          pixels[p + 0] = value;
+          pixels[p + 1] = value;
+          pixels[p + 2] = value;
+      }
+  }
+
+  char filename[20] = {0};
+  sprintf(filename, "images/%d.bmp", seconds);
+
+  FILE *fout = fopen(filename, "wb");
+  fwrite(header, 1, 54, fout);
+  fwrite(pixels, 1, size, fout);
+  free(pixels);
+  fclose(fout);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -40,11 +81,13 @@ int main(int argc, char *argv[]) {
   }
   fclose(fp);
 
-  for (int s = 0; s < 100; s++) {
+  int s;
+  for (s = 0; s < 100; s++) {
     for (int r = 0; r < count; r++) {
       robots[r].c = mod(robots[r].c + robots[r].vc, WIDTH);
       robots[r].r = mod(robots[r].r + robots[r].vr, HEIGHT);
     }
+    generateImage(robots, count, s+1);
   }
 
   int matrix[HEIGHT][WIDTH];
@@ -70,4 +113,12 @@ int main(int argc, char *argv[]) {
     }
   }
   printf("Part 1: %d\n", quads[0] * quads[1] * quads[2] * quads[3]);
+
+  for (s = s; s < 10000; s++) {
+    for (int r = 0; r < count; r++) {
+      robots[r].c = mod(robots[r].c + robots[r].vc, WIDTH);
+      robots[r].r = mod(robots[r].r + robots[r].vr, HEIGHT);
+    }
+    generateImage(robots, count, s+1);
+  }
 }
