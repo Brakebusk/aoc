5f7ccc3578a8ae009db81722e0852b9b545b0cad
Herman Bastesen <hermaba@uio.no>
2024-12-17 09:55:34 +0100

    2024/17 part 2

--- 2024/17/intcode.c
+++ 2024/17/intcode.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int mod(int a, int b) {
+long long mod(long long a, long long b) {
   return (a % b + b) % b;
 }
 
-
-int getCombo(int operand, int a, int b, int c) {
+long long getCombo(long long operand, long long a, long long b, long long c) {
   switch (operand) {
     case 0:
     case 1:
@@ -22,66 +21,23 @@ int getCombo(int operand, int a, int b, int c) {
       return c;
     case 7:
     default:
-      printf("Invalid operand: '%d'\n", operand);
+      printf("Invalid operand: '%lld'\n", operand);
       exit(EXIT_FAILURE);
   }
 }
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("[ERROR] Missing parameter <filename>\n");
-    exit(EXIT_FAILURE);
-  }
-
-  char *filename = argv[1];
-
-  FILE *fp = NULL;
-  if ((fp = fopen(filename, "r")) == NULL) {
-      printf("[ERROR] Failed to open file %s\n", filename);
-      exit(EXIT_FAILURE);
-  }
-
-  int a = 0, b = 0, c = 0;
-  int program[20];
-  int length = 0;
-
-  char line[50];
-  int mode = 0;
-  while(fgets(line, 512, fp)) {
-    char relevant[50];
-    char *token = 0;
-    while ((token = strtok(token ? NULL : line, " "))) {
-      strcpy(relevant, token);
-    }
-    switch (mode) {
-      case 0:
-        a = atoi(relevant);
-        break;
-      case 1:
-        b = atoi(relevant);
-        break;
-      case 2:
-        c = atoi(relevant);
-        break;
-      case 3:
-        break;
-      case 4:
-        token = 0;
-        while ((token = strtok(token ? NULL : relevant, ","))) {
-          program[length++] = atoi(token);
-        }
-        break;
-    }
-    mode++;
-  }
-  fclose(fp);
-
+int runProgram(int program[20], int length, long long initialA, long long initialB, long long initialC, int mode) {
+  if (mode == 2) printf("%lld ", initialA);
+  long long a = initialA, b = initialB, c = initialC;
   int ip = 0;
   int prints = 0;
+  int outBuffer[20] = {0};
+  int bufferLength = 0;
+
   do {
     int opcode = program[ip];
-    int operand = program[ip+1];
-    int combo = getCombo(operand, a, b, c);
+    long long operand = program[ip+1];
+    long long combo = getCombo(operand, a, b, c);
 
     switch (opcode) {
       case 0: // adv => A = A / 2^combo
@@ -101,7 +57,11 @@ int main(int argc, char *argv[]) {
         break;
       case 5: // out => output combo % 8
         if (prints++) printf(",");
-        printf("%d", mod(combo, 8));
+        printf("%lld", mod(combo, 8));
+        if (mode == 2) {
+          outBuffer[bufferLength] = mod(combo, 8);
+          if (++bufferLength > length) return 0;
+        }
         break;
       case 6: // bdv => B = A / 2^combo
         b = a / (combo ? (2 << (combo - 1)) : 1);
@@ -114,5 +74,77 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
   } while ((ip += 2) < length);
+
+  printf(" %d", bufferLength);
+  for (int i = 0; i < length; i++ ) {
+    if (program[i] != outBuffer[i]) return 0;
+  }
+  return length == bufferLength;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("[ERROR] Missing parameter <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  FILE *fp = NULL;
+  if ((fp = fopen(filename, "r")) == NULL) {
+      printf("[ERROR] Failed to open file %s\n", filename);
+      exit(EXIT_FAILURE);
+  }
+
+  long long a = 0, b = 0, c = 0;
+  int program[20];
+  int length = 0;
+
+  char line[50];
+  int mode = 0;
+  while(fgets(line, 512, fp)) {
+    char relevant[50];
+    char *token = 0;
+    while ((token = strtok(token ? NULL : line, " "))) {
+      strcpy(relevant, token);
+    }
+    switch (mode) {
+      case 0:
+        a = atoll(relevant);
+        break;
+      case 1:
+        b = atoll(relevant);
+        break;
+      case 2:
+        c = atoll(relevant);
+        break;
+      case 3:
+        break;
+      case 4:
+        token = 0;
+        while ((token = strtok(token ? NULL : relevant, ","))) {
+          program[length++] = atoi(token);
+        }
+        break;
+    }
+    mode++;
+  }
+  fclose(fp);
+
+  printf("Part 1: ");
+  runProgram(program, length, a, b, c, 1);
   printf("\n");
+
+  printf("                ");
+  for (int i = 0; i < length; i++) {
+    if (i) printf(",");
+    printf("%d", program[i]);
+  }
+  printf("\n");
+  long long test = 0b100101010110100100110000111011010110111010110100;
+  while (runProgram(program, length, test, b, c, 2) == 0) {
+    printf("\n");
+    test+=1;
+  }
+  printf("\nPart 2: %lld\n", test);
 }
