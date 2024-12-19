f7e7cb7421c3d48fb973c679f166b5bb8a113000
Herman Bastesen <hermaba@uio.no>
2024-12-19 14:08:22 +0100

    2020/04 part 2

--- 2020/04/passport.c
+++ 2020/04/passport.c
@@ -10,6 +10,40 @@ int validate(struct passport p) {
   return p.byr[0] && p.iyr[0] && p.eyr[0] && p.hgt[0] && p.hcl[0] && p.ecl[0] && p.pid[0];
 }
 
+int strictValidate(struct passport p) {
+  if (strlen(p.byr) != 4 || strtol(p.byr, NULL, 10) < 1920 || strtol(p.byr, NULL, 10) > 2002) return 0;
+  if (strlen(p.iyr) != 4 || strtol(p.iyr, NULL, 10) < 2010 || strtol(p.iyr, NULL, 10) > 2020) return 0;
+  if (strlen(p.eyr) != 4 || strtol(p.eyr, NULL, 10) < 2020 || strtol(p.eyr, NULL, 10) > 2030) return 0;
+  if (strlen(p.hgt) < 3) return 0;
+
+  char suffix[8] = {0};
+  int value = 0;
+  sscanf(p.hgt, "%d%s", &value, suffix);
+  if (strcmp(suffix, "in") == 0) {
+    if (value < 59 || value > 76) return 0;
+  } else if (strcmp(suffix, "cm") == 0) {
+    if (value < 150 || value > 193) return 0;
+  } else {
+    return 0;
+  }
+
+  char color[16] = {0};
+  sscanf(p.hcl, "#%s", color);
+  if (strlen(color) != 6) return 0;
+
+  if (!(strcmp(p.ecl, "amb") == 0 || 
+        strcmp(p.ecl, "blu") == 0 || 
+        strcmp(p.ecl, "brn") == 0 || 
+        strcmp(p.ecl, "gry") == 0 || 
+        strcmp(p.ecl, "grn") == 0 || 
+        strcmp(p.ecl, "hzl") == 0 || 
+        strcmp(p.ecl, "oth") == 0)) return 0;
+  
+  if (strlen(p.pid) != 9 || strtol(p.pid, NULL, 10) < 1) return 0;
+
+  return 1;
+}
+
 void reset(struct passport *p) {
   p->byr[0] = '\0';
   p->iyr[0] = '\0';
@@ -39,11 +73,13 @@ int main(int argc, char *argv[]) {
   reset(&p);
 
   int part1 = 0;
+  int part2 = 0;
 
   char line[128];
   while(fgets(line, 128, fp)) {
     if (strlen(line) == 1) {
       part1 += validate(p);
+      part2 += strictValidate(p);
       reset(&p);
     } else {
       char *token = 0;
@@ -76,6 +112,8 @@ int main(int argc, char *argv[]) {
   }
   fclose(fp);
   part1 += validate(p);
+  part2 += strictValidate(p);
   
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %d\n", part2);
 }
