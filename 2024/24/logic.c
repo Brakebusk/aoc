ac849bdfbc88192a8c984161b27ebb6f7cc6867b
Herman Bastesen <hermaba@uio.no>
2024-12-24 11:36:46 +0100

    2024/24 part 1

---
+++ 2024/24/logic.c
@@ -0,0 +1,156 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define NODELIM 512
+
+struct node {
+  char name[4];
+  char value;
+};
+
+// types: 0 => AND, 1 => OR, 2 => XOR
+struct gate {
+  char a[4], b[4], type, res[4], evaluated;
+};
+
+int compareNodes(const void *a, const void *b) {
+  struct node node1 = *(struct node*)a;
+  struct node node2 = *(struct node*)b;
+
+  return strcmp(node1.name, node2.name);
+}
+
+int findNode(struct node nodes[NODELIM], int nodeCount, char name[4]) {
+  for (int i = 0; i < nodeCount; i++) {
+    if (strcmp(name, nodes[i].name) == 0) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+long long setBit(long long num, int index) {
+  return num | (1LL << index);
+}
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("[ERROR] Missing parameter <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  FILE *fp = NULL;
+  if ((fp = fopen(filename, "r")) == NULL) {
+      printf("[ERROR] Failed to open file %s\n", filename);
+      exit(EXIT_FAILURE);
+  }
+  
+  struct node nodes[NODELIM];
+  int nodeCount = 0;
+  struct gate gates[256];
+  int gateCount = 0;
+
+  char line[32];
+  int mode = 0;
+  while(fgets(line, 32, fp)) {
+    if (strlen(line) <= 1) {
+      mode = 1;
+    } else {
+      if (mode == 0) {
+        char name[4];
+        int value;
+        sscanf(line, "%3s: %d", name, &value);
+        strcpy(nodes[nodeCount].name, name);
+        nodes[nodeCount++].value = value;
+      } else {
+        char *token = strtok(line, "-");
+        char *rest = token;
+        char *segment;
+        int c = 0;
+        while ((segment = strtok_r(rest, " ", &rest))) {
+          if (c == 0) {
+            strcpy(gates[gateCount].a, segment);
+          } else if (c == 1) {
+            if (strcmp(segment, "AND") == 0) {
+              gates[gateCount].type = 0;
+            } else if (strcmp(segment, "OR") == 0) {
+              gates[gateCount].type = 1;
+            } else if (strcmp(segment, "XOR") == 0) {
+              gates[gateCount].type = 2;
+            } else {
+              printf("Unknown type: '%s'\n", segment);
+              exit(EXIT_FAILURE);
+            }
+          } else {
+            strcpy(gates[gateCount].b, segment);
+          }
+          c++;
+        }
+        token = strtok(NULL, " ");
+        token = strtok(NULL, " \n");
+        gates[gateCount].evaluated = 0;
+        strcpy(gates[gateCount++].res, token);
+      }
+    }
+  }
+  fclose(fp);
+
+  int found = 1;
+  while (found) {
+    found = 0;
+
+    for (int i = 0; i < gateCount; i++) {
+      if (gates[i].evaluated == 0) {
+        int a = findNode(nodes, nodeCount, gates[i].a);
+        int b = findNode(nodes, nodeCount, gates[i].b);
+        if (a > -1 && b > -1) {
+          if (nodeCount == NODELIM) {
+            printf("Too many nodes\n");
+            exit(EXIT_FAILURE);
+          }
+
+          found = 1;
+          char aVal = nodes[a].value;
+          char bVal = nodes[b].value;
+          char type = gates[i].type;
+
+          char resVal;
+          switch (type) {
+            case 0:
+              resVal = aVal & bVal;
+              break;
+            case 1:
+              resVal = aVal | bVal;
+              break;
+            case 2:
+              resVal = aVal ^ bVal;
+              break;
+            default:
+              printf("Unknown type eval: '%d'\n", type);
+              exit(EXIT_FAILURE);
+          }
+
+          strcpy(nodes[nodeCount].name, gates[i].res);
+          nodes[nodeCount++].value = resVal;
+
+          gates[i].evaluated = 1;
+        }
+      }
+    }
+  }
+
+  qsort(nodes, nodeCount, sizeof(struct node), compareNodes);
+
+  long long part1 = 0;
+
+  for (int i = 0; i < nodeCount; i++) {
+    if (nodes[i].name[0] == 'z' && nodes[i].value) {
+      int index = atoi(&nodes[i].name[1]);
+      part1 = setBit(part1, index);
+    }
+  }
+  printf("Part 1: %lld\n", part1);
+}
