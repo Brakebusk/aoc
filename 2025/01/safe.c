50622cf2440f54ae6cfc5bb1c5913b15f0f65f6e
Herman Bastesen <hermaba@uio.no>
2025-12-01 08:41:07 +0100

    2025/01 part 1

---
+++ 2025/01/safe.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int mod(int a, int b) {
+  return (a % b + b) % b;
+}
+
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    printf("[ERROR] Missing parameter <filename>\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char *filename = argv[1];
+
+  FILE *fp = NULL;
+  if ((fp = fopen(filename, "r")) == NULL) {
+      printf("[ERROR] Failed to open file %s\n", filename);
+      exit(EXIT_FAILURE);
+  }
+
+  int dial = 50;
+  int part1 = 0;
+
+  char line[8];
+  while(fgets(line, 8, fp)) {
+    char dir;
+    int distance;
+    sscanf(line, "%c%d", &dir, &distance);
+
+    if (dir == 'L') {
+      dial -= distance;
+    } else if (dir == 'R') {
+      dial += distance;
+    }
+    if (mod(dial, 100) == 0) part1++;
+  }
+  fclose(fp);
+
+  printf("Part 1: %d\n", part1);
+}
