f79dc7d32e28b1c6477f4f4d38b1e0e870ca5236
Herman Bastesen <hermaba@uio.no>
2025-12-05 08:54:26 +0100

    2025/05 part 2

--- 2025/05/fresh.c
+++ 2025/05/fresh.c
@@ -2,9 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-int isWithinRange(long long ranges[200][2], int rangeCount, long long check) {
+struct range {
+  long long min;
+  long long max;
+};
+
+int compare(const void *a, const void* b) {
+  struct range range1 = *(struct range*)a;
+  struct range range2 = *(struct range*)b;
+
+  if (range1.min > range2.min) {
+    return 1;
+  } else if (range1.min < range2.min) {
+    return -1;
+  } else return range1.max - range2.max;
+}
+
+int isWithinRange(struct range ranges[200], int rangeCount, long long check) {
   for (int r = 0; r < rangeCount; r++) {
-    if (check >= ranges[r][0] && check <= ranges[r][1]) return 1;
+    if (check >= ranges[r].min && check <= ranges[r].max) return 1;
   }
   return 0;
 }
@@ -25,7 +41,7 @@ int main(int argc, char *argv[]) {
 
   int part1 = 0;
 
-  long long ranges[200][2];
+  struct range ranges[200];
   int rangeCount = 0;
 
   char line[64];
@@ -33,11 +49,12 @@ int main(int argc, char *argv[]) {
   while(fgets(line, 64, fp)) {
     if (line[0] == '\n') {
       mode = 2;
+      qsort(ranges, rangeCount, sizeof(struct range), compare);
       continue;
     }
 
     if (mode == 1) {
-      sscanf(line, "%lld-%lld", &ranges[rangeCount][0], &ranges[rangeCount][1]);
+      sscanf(line, "%lld-%lld", &ranges[rangeCount].min, &ranges[rangeCount].max);
       rangeCount++;
     } else {
       long long check = atoll(line);
@@ -47,4 +64,19 @@ int main(int argc, char *argv[]) {
   fclose(fp);
 
   printf("Part 1: %d\n", part1);
+
+  long long part2 = 0;
+  
+  long long pointer = ranges[0].max;
+  part2 += 1 + pointer - ranges[0].min;
+  
+  for (int r = 1; r < rangeCount; r++) {
+    struct range selected = ranges[r];
+    if (selected.min > pointer) pointer = selected.min;
+    if (selected.max < pointer) continue;
+    part2 += 1 + selected.max - pointer;
+    pointer = 1 + selected.max;
+  }
+
+  printf("Part 2: %lld\n", part2);
 }
