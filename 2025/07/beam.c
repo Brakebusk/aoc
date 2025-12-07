1d48c5090e57ea2af63d034a8ad77cf235368167
hermaba <hermaba@uio.no>
2025-12-07 11:44:00 +0100

    2025/07 part 2

--- 2025/07/beam.c
+++ 2025/07/beam.c
@@ -2,6 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+long long countPaths(char matrix[150][150], long long cache[150][150], int size, int r, int c) {
+  if (cache[r][c]) return cache[r][c];
+  if (r+1 == size) return 1;
+  if (matrix[r+1][c] == '.') {
+    cache[r+1][c] = countPaths(matrix, cache, size, r+1, c);
+    return cache[r+1][c];
+  }
+  if (matrix[r+1][c] == '^') {
+    cache[r+1][c-1] = countPaths(matrix, cache, size, r+1, c-1);
+    cache[r+1][c+1] = countPaths(matrix, cache, size, r+1, c+1);
+    return cache[r+1][c-1] + cache[r+1][c+1];
+  }
+  printf("Invalid character encountered: %c\n", matrix[r+1][c]);
+  exit(EXIT_FAILURE);
+} 
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("[ERROR] Missing parameter <filename>\n");
@@ -27,9 +43,14 @@ int main(int argc, char *argv[]) {
   }
   fclose(fp);
 
+  long long part2;
+  long long cache[150][150];
+  memset(cache, 0, sizeof(long long) * 150 * 150);
+
   for (int c = 0; c < size; c++) {
     if (matrix[0][c] == 'S') {
       matrix[1][c] = '|';
+      part2 = countPaths(matrix, cache, size, 1, c);
       break;
     }
   }
@@ -51,4 +72,5 @@ int main(int argc, char *argv[]) {
   }
 
   printf("Part 1: %d\n", part1);
+  printf("Part 2: %lld\n", part2);
 }
